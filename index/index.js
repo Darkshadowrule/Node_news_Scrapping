876b9cd61dab4e65bb0e1f778eb4a0a2d053d8b7
darkshadowrule <prajjwaldxi3@gmail.com>
2019-03-15 16:41:40 +0530

    News Scrapping

---
+++ index/index.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const Table = require('cli-table');
+const cheerio = require('cheerio');
+
+
+var table = new Table({
+    head: ['Heading', 'Url']
+  , colWidths: [130, 190]
+});
+
+var url='https://news.ycombinator.com';
+request(url,(err,res,body)=>{
+  if(!err)
+  {
+    var $ = cheerio.load(body);
+     $('.athing').each(function(i){
+       var a = $(this).children();
+       var b=$('.storylink').eq(i)
+       table.push([a.text().toString(),b.attr('href').toString()]);
+     });
+}
+console.log(table.toString());
+})
